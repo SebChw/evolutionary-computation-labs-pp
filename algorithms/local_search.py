6920fb273812dbb9ebd10797c503416176ef2772
SebChw <sebastian.chwilczynski@gmail.com>
2023-11-18 13:54:02 +0100

    almost working

--- algorithms/local_search.py
+++ algorithms/local_search.py
@@ -2,7 +2,7 @@ import enum
 from copy import copy
 from itertools import product
 from queue import PriorityQueue
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 
@@ -91,9 +91,9 @@ class LocalSearch:
         else:
             self.steepest()
 
-        # print(
-        #     f"Final solution cost: {calculate_path_cost(self.solution, self.adj_matrix, self.nodes_cost)}"
-        # )
+        print(
+            f"Final solution cost: {calculate_path_cost(self.solution, self.adj_matrix, self.nodes_cost)}"
+        )
         return self.solution
 
     def generate_pairs_to_check(self, n_indices: int, m_indices: int):
@@ -282,48 +282,84 @@ class LocalSearch:
             else:
                 self.update_solution(best_move)
 
-    def get_neighbors(self, node_id):
+    def get_neighbors(self, node_id: int) -> Tuple[int, int]:
+        """I used this a lot so I made a function out of this
+
+        Args:
+            node_id (int): index of the node in the solution
+
+        Returns:
+            Tuple[int, int]: Actual nodes values of neighbors
+        """
+
         return (
             self.solution[node_id - 1],
             self.solution[(node_id + 1) % len(self.solution)],
         )
 
-    def add_priority_inter(self, i, node_j, delta):
-        left_n, right_n = self.get_neighbors(i)
-        #! For every move we need to know exactly what edges would be added and what removed
-        #! If removed nodes are no longer in the solution -> We can't apply it
-        removed = [(left_n, self.solution[i]), (self.solution[i], right_n)]
+    def add_priority_inter(self, i: int, node_j: int, delta: int):
+        """
+        The deal is that we can't just remember relative movements. As our solution changes, relative positions of nodes may change
 
+        WE MUST remember actual nodes and edges that were removed
+
+        Args:
+            i (int): index of the node in the solution
+            node_j (int): node that we want to add to the solution
+            delta (int): delta of this move
+        """
+        left_n, right_n = self.get_neighbors(i)
+        node_i = self.solution[i]
+        # This edges will be removed if we perform this move
+        removed = [(left_n, node_i), (node_i, right_n)]
+
+        # Based on changed field we will be later able to infer relative positions of nodes to make a move
         element = (
             -delta,
+            np.random.rand(),  # We must provide some way of ordering when delta is the same, with dict we have exception about '<' not being supported
             (
                 {
                     "removed": removed,
-                    "move": (i, node_j, INTER),
+                    "changed": [node_i, node_j],
                     "change_type": INTER,
                 }
             ),
         )
         self.prioritized_moves.put(element)
 
-    def add_priority_intra(self, i, j, delta):
+    def add_priority_intra(self, i: int, j: int, delta: int):
+        """
+        The deal is that we can't just remember relative movements. As our solution changes, relative positions of nodes may change
+
+        WE MUST remember actual nodes and edges that were removed
+
+        Args:
+            i (int): index of the node in the solution
+            j (int): index of the node in the solution
+            delta (int): delta of this move
+        """
         left_i, right_i = self.get_neighbors(i)
         left_j, right_j = self.get_neighbors(j)
         node_i, node_j = self.solution[i], self.solution[j]
         if i > j:
-            #
+            # we go from j to j
             removed = [(left_i, node_i), (left_j, node_j)]
-            # TBD
-            # added =
         else:
+            # we go from i to j
             removed = [(node_i, right_i), (node_j, right_j)]
-            # TBD
-            # added = These will actually dynamically change. Just the removed nodes stay the same. But this means delta will change too.
 
-        #! I can't use this "move" I must operate on actual nodes
         element = (
             -delta,
-            ({"removed": removed, "move": (i, j, INTRA), "change_type": INTRA}),
+            # We must provide some way of ordering when delta is the same, with dict we have exception about '<' not being supported
+            # I added i > j to have always the some order when delta is the same
+            int(i > j) + np.random.rand(),
+            (
+                {
+                    "removed": removed,
+                    "changed": (node_i, node_j),
+                    "change_type": INTRA,
+                }
+            ),
         )
         self.prioritized_moves.put(element)
 
@@ -334,11 +370,13 @@ class LocalSearch:
             self.node_to_position[node] = i
 
     def prepare_for_move_eval(self):
+        """
+        During the preparation we inverstigate entire neighbourhood of our solution and add all improving moves to the queue
+        """
         self.prioritized_moves = PriorityQueue()
         self.update_node_to_position()
 
         # At first we need to evaluate all moves. Leter we will just update this list with newest
-        #! We can't operate on abstract indices now, we must operate on real nodes and edges
         for i, node_j, delta in self.inter_route_exchange_deltas(self.solution):
             if delta > 0:
                 self.add_priority_inter(i, node_j, delta)
@@ -347,22 +385,37 @@ class LocalSearch:
             if delta > 0:
                 self.add_priority_intra(i, j, delta)
 
-    def get_removed_case(self, removed, change_type):
+    def get_removed_case(
+        self, removed: List[Tuple[int, int]], change_type: str
+    ) -> LMCase:
+        """Here we decide what should be done with a move that was suggested to be the best
+
+        These function tree to cover 3 cases given by the tutor
+
+        Args:
+            removed (List[Tuple[int,int]]): Which edges would be removed from the solution
+            change_type (str): Is it Inter or intra change
+
+        Returns:
+            LMCase: Decision on what we should do
+        """
         for edge in removed:
             node_i, node_j = edge
+            # Edge doesn't exist -  nodes are not in the solution
             if (
                 self.node_to_position[node_i] == -1
                 or self.node_to_position[node_j] == -1
             ):
                 return LMCase.NOT_EXISTING
-
+            # Edge doesn't exist - nodes are not neighbours
             node_i_position = self.node_to_position[node_i]
             node_j_position = self.node_to_position[node_j]
             if abs(node_i_position - node_j_position) > 1:
                 return LMCase.NOT_EXISTING
 
-        # We assume that these edges are in the same direction as in the solution
-        relative = (True, True)
+        # At this point we know that both nodes are in the solution and they are neighbours
+        # But did the direction of this neighbourhood change?
+        relative = [True, True]
         for i, edge in enumerate(removed):
             node_i, node_j = edge
             node_i_position = self.node_to_position[node_i]
@@ -379,58 +432,108 @@ class LocalSearch:
         else:
             return LMCase.REVERSED
 
-    def add_new_to_priorietized_moves(self, best_move):
+    def add_new_to_priorietized_moves(
+        self, best_move: Tuple[int, int, str], removed: List[Tuple[int, int]]
+    ):
+        """Every time our solution changes we need to add new moves to our queue
+
+        Args:
+            best_move (Tuple[int, int, str]): best move - relative
+            removed (List[Tuple[int, int]]): True Indices of edges that were removed
+        """
         change_type = best_move[2]
         not_selected = self.ALL_INDICES_SET - set(self.solution)
         if change_type == INTER:
             i = best_move[0]
 
-            #! ADDING NEW INTER MOVES - but is it actually necessar. No other node exchange would bring better delta back.
-            # node_i = self.solution[i]
-            # neighbor_i_l, neighbor_i_r = (
-            #     self.solution[i - 1],
-            #     self.solution[(i + 1) % len(self.solution)],
-            # )
+            # ADDING NEW INTER MOVES - DELTAS CHANGES FOR NEIGHBORS OF NEWLY ADDED NODE
+            for neigh_i in [-1, 1]:
+                neigh_i = (i + neigh_i) % len(self.solution)
 
-            # for node_j in not_selected:
-            #     curr_len, len_after_change = self.compute_distances_inter(
-            #         node_i, node_j, neighbor_i_l, neighbor_i_r
-            #     )
-            #     delta = curr_len - len_after_change
-            #     if delta > 0:
-            #         self.add_priority_inter(i, node_j, delta)
-            #! ADDING NEW INTRA MOVES
-            for j in range(len(self.solution)):
-                if abs(i - j) <= 1 or (i == 99 and j == 0):
-                    continue
-                delta_i_j = self.compute_delta_edges_correct(i, j)
-                if delta_i_j > 0:
-                    self.add_priority_intra(i, j, delta_i_j)
-                delta_j_i = self.compute_delta_edges_correct(j, i)
-                if delta_j_i > 0:
-                    self.add_priority_intra(j, i, delta_j_i)
+                neighbor_i_l, neighbor_i_r = self.get_neighbors(neigh_i)
+                for node_j in not_selected:
+                    curr_len, len_after_change = self.compute_distances_inter(
+                        self.solution[neigh_i], node_j, neighbor_i_l, neighbor_i_r
+                    )
+                    delta = curr_len - len_after_change
+                    if delta > 0:
+                        self.add_priority_inter(neigh_i, node_j, delta)
+
+            # ADDING NEW INTRA MOVES - we have new edges so deltas will change
+            for neigh_i in [-1, 1, 0]:
+                neigh_i = (i + neigh_i) % len(self.solution)
+                for j in range(len(self.solution)):
+                    if abs(neigh_i - j) <= 1 or (neigh_i == 99 and j == 0):
+                        continue
+                    delta_i_j = self.compute_delta_edges_correct(neigh_i, j)
+                    if delta_i_j > 0:
+                        self.add_priority_intra(neigh_i, j, delta_i_j)
+                    delta_j_i = self.compute_delta_edges_correct(j, neigh_i)
+                    if delta_j_i > 0:
+                        self.add_priority_intra(j, neigh_i, delta_j_i)
         else:
-            #! ADDING NEW INTER MOVES we didn't introduce new node. However deltas have changes
-            for node_id in best_move[:2]:
-                node_i = self.solution[node_id]
-                neighbor_i_l, neighbor_i_r = (
-                    self.solution[node_id - 1],
-                    self.solution[(node_id + 1) % len(self.solution)],
-                )
+            # ADDING NEW INTER MOVES we didn't introduce new node. However deltas have changes
+            # Situation changes for all nodes that edges has been removed
+            nodes_with_changed_situation = [
+                removed[0][0],
+                removed[0][1],
+                removed[1][0],
+                removed[1][1],
+            ]
+            for node_i in nodes_with_changed_situation:
+                i = self.node_to_position[node_i]
+                neighbor_i_l, neighbor_i_r = self.get_neighbors(i)
 
-            for node_j in not_selected:
-                curr_len, len_after_change = self.compute_distances_inter(
-                    node_i, node_j, neighbor_i_l, neighbor_i_r
-                )
-                delta = curr_len - len_after_change
-                if delta > 0:
-                    self.add_priority_inter(i, node_j, delta)
+                for node_j in not_selected:
+                    curr_len, len_after_change = self.compute_distances_inter(
+                        node_i, node_j, neighbor_i_l, neighbor_i_r
+                    )
+                    delta = curr_len - len_after_change
+                    if delta > 0:
+                        self.add_priority_inter(i, node_j, delta)
 
-            #! ADDING NEW INTRA MOVES - again not necessary as only new possible option is to reverse what we have done?
+            # ADDING NEW INTRA MOVES - only nodes that were in removed edges are affected
+            for node_i in nodes_with_changed_situation:
+                neigh_i = self.node_to_position[node_i]
+                for j in range(len(self.solution)):
+                    if abs(neigh_i - j) <= 1 or (neigh_i == 99 and j == 0):
+                        continue
+                    delta_i_j = self.compute_delta_edges_correct(neigh_i, j)
+                    if delta_i_j > 0:
+                        self.add_priority_intra(neigh_i, j, delta_i_j)
+                    delta_j_i = self.compute_delta_edges_correct(j, neigh_i)
+                    if delta_j_i > 0:
+                        self.add_priority_intra(j, neigh_i, delta_j_i)
+
+    def infer_best_move(
+        self, changed: List[int], change_type: str
+    ) -> Tuple[int, int, str]:
+        """Given original node indices (change) we must find relative positions of nodes in the solution to generate a move
+
+        Args:
+            changed (List[int]): Original node indices that take part in the move
+            change_type (str): move
+
+        Returns:
+            Tuple[int, int, str]: move in relative terms
+        """
+        if change_type == INTER:
+            i = self.node_to_position[changed[0]]
+            node_j = changed[1]
+            return i, node_j, INTER  # position, actual node
+
+        else:
+            node_i, node_j = changed
+            i = self.node_to_position[node_i]
+            j = self.node_to_position[node_j]
+            return i, j, INTRA  # position, position
 
     def steepest(
         self,
     ):
+        old_cost = calculate_path_cost(self.solution, self.adj_matrix, self.nodes_cost)
+        old_solution = None
+
         # Preparations before strategy
         if self.strategy == LSStrategy.STEEPEST_CANDIDATE:
             self.construct_candidate_list()
@@ -495,21 +598,28 @@ class LocalSearch:
                                 best_move = j, i, INTRA
 
             elif self.strategy == LSStrategy.STEEPEST_MOVE_EVAL:
+                # We need to put back some moves, that can be reused late
                 to_put_back = []
-                while self.prioritized_moves.empty():
-                    delta, move_dict = self.prioritized_moves.get()
-                    removed, move = move_dict["removed"], move_dict["move"]
+                while not self.prioritized_moves.empty():
+                    delta, random_value, move_dict = self.prioritized_moves.get()
+                    # removed - removed edges
+                    # changed - nodes that take part in a move
+                    removed, changed = move_dict["removed"], move_dict["changed"]
                     change_type = move_dict["change_type"]
 
+                    # Case when we want to add node that already exists in the solution
+                    if change_type == INTER and self.node_to_position[changed[1]] != -1:
+                        continue
+
                     lm_case = self.get_removed_case(removed, change_type)
-                    # Check if move is not applicable
                     if lm_case == LMCase.NOT_EXISTING:
                         continue
                     elif lm_case == LMCase.REVERSED:
-                        to_put_back.append((delta, move_dict))
-                    # Check if move may be applicable
+                        to_put_back.append((delta, random_value, move_dict))
                     else:
-                        best_move = move
+                        best_delta = delta
+                        move_dict["move"] = self.infer_best_move(changed, change_type)
+                        best_move = move_dict
                         break
 
                 # Maybe in the next iteration we use them
@@ -534,11 +644,32 @@ class LocalSearch:
             if best_move is None:
                 break
             else:
-                self.update_solution(best_move)
+                if self.strategy == LSStrategy.STEEPEST_MOVE_EVAL:
+                    old_solution = copy(self.solution)
+                    self.update_solution(best_move["move"])
+                else:
+                    self.update_solution(best_move)
 
             if self.strategy == LSStrategy.STEEPEST_MOVE_EVAL:
-                self.add_new_to_priorietized_moves(best_move)
+                # After updating solution our nodes may completely change their positions
                 self.update_node_to_position()
+
+                # We need to add new moves to our queue
+                self.add_new_to_priorietized_moves(
+                    best_move["move"], best_move["removed"]
+                )
+                #! Rarely deltas were inconsistent and I couldn't find the reason
+                # new_cost = calculate_path_cost(
+                #     self.solution, self.adj_matrix, self.nodes_cost
+                # )
+                # if new_cost != old_cost + best_delta:
+                #     self.solution = old_solution
+                #     print("ROLLBACK")
+                # self.update_node_to_position()
+                # old_cost = new_cost
+
+                # Check if our solution is valid
+                # assert len(set(self.solution)) == 100
 
     def update_solution(self, best_move):
         type_ = best_move[2]
--- evaluate_ls.py
+++ evaluate_ls.py
@@ -8,27 +8,44 @@ import numpy as np
 from joblib import Parallel, delayed
 
 from algorithms.greedy_2_regret import Greedy2Regret
-from algorithms.local_search import LocalSearch
+from algorithms.local_search import LocalSearch, LSStrategy
 from algorithms.random import random_hamiltonian
 from algorithms.utils import Solution, calculate_path_cost
 from data.data_parser import get_data
 
 
-def perform_local_search(greedy, exchange_nodes, starting_solution_name, starting_solution, distance_matrix, nodes_cost, candidate_moves):
+def perform_local_search(
+    greedy,
+    exchange_nodes,
+    starting_solution_name,
+    starting_solution,
+    distance_matrix,
+    nodes_cost,
+    candidate_moves,
+):
     local_search_start_time = time.perf_counter()
     local_search = LocalSearch(
-        greedy=greedy, exchange_nodes=exchange_nodes, candidate_moves=candidate_moves)
-    result = local_search(distance_matrix, nodes_cost,
-                          deepcopy(starting_solution))
+        greedy=greedy, exchange_nodes=exchange_nodes, candidate_moves=candidate_moves
+    )
+    result = local_search(distance_matrix, nodes_cost, deepcopy(starting_solution))
     solution_dict = asdict(
-        Solution(result, calculate_path_cost(result, distance_matrix, nodes_cost)))
+        Solution(result, calculate_path_cost(result, distance_matrix, nodes_cost))
+    )
     local_search_time = time.perf_counter() - local_search_start_time
-    return greedy, exchange_nodes, starting_solution_name, solution_dict, local_search_time, candidate_moves
+    return (
+        greedy,
+        exchange_nodes,
+        starting_solution_name,
+        solution_dict,
+        local_search_time,
+        candidate_moves,
+    )
 
 
 data = get_data()
-results = defaultdict(lambda: defaultdict(
-    lambda: defaultdict(lambda: defaultdict(list))))
+results = defaultdict(
+    lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+)
 
 for problem, instance in data.items():
     print(problem)
@@ -39,42 +56,57 @@ for problem, instance in data.items():
         starting_solutions = {}
         starting_solution_times = {}
         start_time_random = time.perf_counter()
-        starting_solutions['random'] = random_hamiltonian(
-            distance_matrix, nodes_cost)
-        starting_solution_times['random'] = time.perf_counter(
-        ) - start_time_random
+        starting_solutions["random"] = random_hamiltonian(distance_matrix, nodes_cost)
+        starting_solution_times["random"] = time.perf_counter() - start_time_random
 
         start_time_greedy = time.perf_counter()
-        starting_solutions['greedy'] = Greedy2Regret(alpha=0.5)(
-            distance_matrix, nodes_cost, starting_node=i)
-        starting_solution_times['greedy'] = time.perf_counter(
-        ) - start_time_greedy
+        starting_solutions["greedy"] = Greedy2Regret(alpha=0.5)(
+            distance_matrix, nodes_cost, starting_node=i
+        )
+        starting_solution_times["greedy"] = time.perf_counter() - start_time_greedy
 
-        #! Run all of these after bug was found.
-        tasks.append(delayed(perform_local_search)(False, False, 'random', starting_solutions['random'], distance_matrix, nodes_cost, True))
-        tasks.append(delayed(perform_local_search)(False, False, 'greedy', starting_solutions['greedy'], distance_matrix, nodes_cost, False))
-        tasks.append(delayed(perform_local_search)(False, False, 'random', starting_solutions['random'], distance_matrix, nodes_cost, False))
-        tasks.append(delayed(perform_local_search)(True, False, 'random', starting_solutions['random'], distance_matrix, nodes_cost, False))
-        tasks.append(delayed(perform_local_search)(True, False, 'greedy', starting_solutions['greedy'], distance_matrix, nodes_cost, False))
-
+        local_search = LocalSearch(LSStrategy.STEEPEST_MOVE_EVAL, exchange_nodes=False)
+        result = local_search(
+            distance_matrix, nodes_cost, deepcopy(starting_solutions["random"])
+        )
+        print("LS pure steepest")
+        local_search = LocalSearch(LSStrategy.STEEPEST, exchange_nodes=False)
+        result = local_search(
+            distance_matrix, nodes_cost, deepcopy(starting_solutions["random"])
+        )
+        raise Exception("Finito.")
 
     n_jobs = -1  # Use all available cores
     parallel_results = Parallel(n_jobs=n_jobs)(tasks)
-    
+
     # Process the results
     for result in parallel_results:
-        greedy, exchange_nodes, starting_solution_name, solution_dict, local_search_time, candidate_moves = result
-        greedy_name = 'greedy' if greedy else 'steepest'
-        exchange_nodes_name = 'nodes' if exchange_nodes else 'edges'
-        for x in range(len(solution_dict['nodes'])):
-            solution_dict['nodes'][x] = int(solution_dict['nodes'][x])
-        results[problem][greedy_name][exchange_nodes_name][starting_solution_name].append({
-            'solution': solution_dict,
-            'local_search_time': local_search_time,
-            'starting_solution_time': starting_solution_times[starting_solution_name],
-            'total_time': local_search_time + starting_solution_times[starting_solution_name],
-            "candidate_moves": candidate_moves,
-        })
+        (
+            greedy,
+            exchange_nodes,
+            starting_solution_name,
+            solution_dict,
+            local_search_time,
+            candidate_moves,
+        ) = result
+        greedy_name = "greedy" if greedy else "steepest"
+        exchange_nodes_name = "nodes" if exchange_nodes else "edges"
+        for x in range(len(solution_dict["nodes"])):
+            solution_dict["nodes"][x] = int(solution_dict["nodes"][x])
+        results[problem][greedy_name][exchange_nodes_name][
+            starting_solution_name
+        ].append(
+            {
+                "solution": solution_dict,
+                "local_search_time": local_search_time,
+                "starting_solution_time": starting_solution_times[
+                    starting_solution_name
+                ],
+                "total_time": local_search_time
+                + starting_solution_times[starting_solution_name],
+                "candidate_moves": candidate_moves,
+            }
+        )
 
 
 # Save the results to a JSON file
