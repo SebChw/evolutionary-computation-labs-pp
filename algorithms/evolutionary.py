3189e55900755a0ccde2cd9e634933ce1a092523
SebChw <sebastian.chwilczynski@gmail.com>
2024-01-04 19:58:53 +0100

    add logging

--- algorithms/evolutionary.py
+++ algorithms/evolutionary.py
@@ -1,6 +1,8 @@
+import logging
 import random
 import time
 from collections import namedtuple
+from datetime import datetime
 from typing import Dict, List, Set, Tuple
 
 import numpy as np
@@ -9,6 +11,10 @@ from algorithms.greedy_2_regret import Greedy2Regret
 from algorithms.local_search import LocalSearch, LSStrategy
 from algorithms.random import random_hamiltonian
 from algorithms.utils import calculate_path_cost
+
+logging.basicConfig(
+    filename=f"{str(datetime.now())}.log", encoding="utf-8", level=logging.DEBUG
+)
 
 TSPSolution = namedtuple("TSPSolution", ["solution", "cost"])
 NOT_YET_FOUND = -1
@@ -57,11 +63,19 @@ class HybridEvolutionary:
         if subpaths[-1][-1] == parentA[-1] and parentA[0] in common_nodes:
             # At first check if we can merge last and first subpath, if not append first node to last subpath
             if subpaths[0][0] == parentA[0]:
+                logging.debug(f"rare case 1, subpaths:len {len(subpaths)}")
                 subpaths[0] = subpaths.pop() + subpaths[0]
             else:
+                logging.debug(f"rare case 2, subpaths:len {len(subpaths)}")
                 subpaths[-1].append(parentA[0])
 
         n_nodes_in_subpaths = sum([len(subpath) for subpath in subpaths])
+        if n_nodes_in_subpaths > len(parentA):
+            logging.error(
+                f"Subpaths: {subpaths}\n ParentA: {parentA}\n ParentB: {parentB} \n Common nodes: {common_nodes}"
+            )
+            raise Exception("Subpaths are calculated incorrectly!")
+
         offspring = []
         # Place subpaths in somehow random order. Total random would be very computationally expensive
         random.shuffle(subpaths)
@@ -90,6 +104,7 @@ class HybridEvolutionary:
         n_possible_nodes = self.adj_matrix.shape[0]
         uncommon_nodes = list(set(range(n_possible_nodes)).difference(common_nodes))
         random.shuffle(common_nodes_to_use)
+        random.shuffle(uncommon_nodes)
 
         while len(indices_to_be_filled) > 0:
             index = indices_to_be_filled.pop()
@@ -103,7 +118,11 @@ class HybridEvolutionary:
             else:
                 offspring[index] = uncommon_nodes.pop()
 
-        assert len(set(offspring)) == len(offspring)
+        if len(set(offspring)) != len(offspring):
+            logging.error(
+                f"Offspring: {offspring}\n ParentA: {parentA}\n ParentB: {parentB} \n Common nodes: {common_nodes}\n Common nodes to use: {list(common_nodes.difference(already_used_nodes))}\n Uncommon nodes: {list(set(range(n_possible_nodes)).difference(common_nodes))} "
+            )
+            raise Exception("Duplicate nodes in offspring!")
         return offspring
 
     def repair_crossover(self, parentA, parentB) -> List[int]:
